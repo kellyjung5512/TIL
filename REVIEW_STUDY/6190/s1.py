98e1b307469fd27c4805e73cb88a3bb57a10bd5c
kellyjung <jung55120@naver.com>
2021-09-15 21:54:41 +0900

    TIL 0915

--- REVIEW_STUDY/6190/s1.py
+++ REVIEW_STUDY/6190/s1.py
@@ -5,7 +5,17 @@ T = int(input())
 for tc in range(1, 1+T):
     n = int(input())
     my_num = list(map(int, input().split()))
-    for i in range(n):
-        
+    num_list = []
+    for i in range(n-1):
+
+        check = list(str(my_num[i] * my_num[i+1]))
+        # print(check)
+        for j in range(len(check)-1):
+            if int(check[j]) <= int(check[j+1]):
+                num_list.append(my_num[i] * my_num[i+1])
     # print(my_num)
-    # print('#{0}'.format(tc))
+    num_list.sort()
+    if num_list :
+        print('#{0} {1}'.format(tc, num_list[-1]))
+    else:
+        print('#{0} {1}'.format(tc, -1))
