31b7c49ec09e9bc90058d81a01778496383e9567
kellyjung <jung55120@naver.com>
2021-09-15 21:38:40 +0900

    TIL 0915

--- REVIEW_STUDY/4613/s1.py
+++ REVIEW_STUDY/4613/s1.py
@@ -6,5 +6,27 @@ for tc in range(1, T+1):
     n, m = map(int, input().split())
 
     my_flag = [list(map(str, input())) for _ in range(n)]
+    new_flag = my_flag[:]
+    # print(n, m)
+    # print(new_flag)
+    # print(my_flag)
+    min_num = n * m
+    for i in range(0, n-2):
+        for j in range(i+1, n-1):
+            cnt = 0
+            for w in my_flag[:i+1]:
+                for k in w:
+                    if k != "W":
+                        cnt += 1
+            for b in my_flag[i+1:j+1]:
+                for k in b:
+                    if k != "B":
+                        cnt += 1
+            for r in my_flag[j+1:]:
+                for k in r:
+                    if k != "R":
+                        cnt += 1
+            if cnt < min_num:
+                min_num = cnt
 
-    print(my_flag)
+    print(min_num)
---
+++ REVIEW_STUDY/6190/s1.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, 1+T):
+    n = int(input())
+    my_num = list(map(int, input().split()))
+    for i in range(n):
+        
+    # print(my_num)
+    # print('#{0}'.format(tc))
