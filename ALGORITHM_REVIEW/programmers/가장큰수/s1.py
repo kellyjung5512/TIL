1e1469ecbc8345471d559de102ee0755e92b7d42
kellyjung5512 <jung55120@naver.com>
2022-01-08 11:59:36 +0900

    TIL 0108

--- ALGORITHM_REVIEW/programmers/가장큰수/s1.py
+++ ALGORITHM_REVIEW/programmers/가장큰수/s1.py
@@ -5,12 +5,10 @@ def solution(numbers):
     if len(numbers) > 0:
         # check = [i for i in range(len(numbers))]
         check_ans = list(itertools.permutations(numbers))
-        # print(check_ans)
         for i in range(len(check_ans)):
             temp = ''
             for j in range(len(check_ans[i])):
                 temp += str(check_ans[i][j])
-            # print(temp)
             if temp > answer:
                 answer = temp
     else:
