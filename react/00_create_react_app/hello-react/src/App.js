b86e872b0bee44e41a59f1284d9308c84a62e032
kellyjung5512 <jung55120@naver.com>
2022-01-18 08:20:14 +0900

    TIL 0118

---
+++ react/00_create_react_app/hello-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = '리액트';
+  return (
+    <>
+      <div className="react">{name}</div>
+      <input></input>
+    </>
+  );
+}
+
+export default App;
