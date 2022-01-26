3ed6a37bc4e92ca72fa7041005f80f49fcc84156
kellyjung <jung55120@naver.com>
2022-01-26 21:31:13 +0900

    TIL 0126

--- 00_practice/react/00_create_react_app/tutorial/src/App.js
+++ 00_practice/react/00_create_react_app/tutorial/src/App.js
@@ -1,29 +1,18 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import Button from "@material-ui/core/Button";
-import Card from "./Card.js"
+import Jumbotron from './Jumbotron'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="text">Text</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Card></Card>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+	render() {
+	  return (
+		<div className="App">
+			<Jumbotron 
+				title={'예발자닷컴'} 
+				content={'예비개발자를 위한 무료 부트캠프 코스를 한 눈에 비교하세요.'}>
+			</Jumbotron>
+		</div>
+	  );
+	}
+  }
 
-export default App;
+  export default App;
