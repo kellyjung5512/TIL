53221a8411b8b17c21149e43fd8bd0398c46a44e
kellyjung <jung55120@naver.com>
2022-02-27 16:58:46 +0900

    TIL 0227

--- 01_algorithm_study/baekjoon/Practice/1700_멀티탭/1700.py
+++ 01_algorithm_study/baekjoon/Practice/1700_멀티탭/1700.py
@@ -1,10 +1,11 @@
 import sys
 sys.stdin = open("input.txt")
 
-hole, times = map(int, input().split())
-equip = list(map(int, input().split()))
+hole, times = map(int, input().split())  # hole == 멀티탭 개수, times == 꼽아야 하는 장치의 개수
+equip = list(map(int, input().split()))  # equip == 장치 리스트
 
-multitap = []
+multitap = []  # multitap == 멀티탭
+cnt = 0
 
 for i in range(len(equip)):
     if len(multitap) < hole:
@@ -14,7 +15,18 @@ for i in range(len(equip)):
     elif equip[i] in multitap:
         continue
 
-    idx_box = []
+    equip_idx = 101
+    out_list = []
     for j in range(hole):
-        if multitap[j] in equip:
-            # 어떻게 할까 고민
+        if multitap[j] in equip[i:]:
+            equip_idx = equip[i:].index(multitap[j])
+
+        out_list.append(equip_idx)
+
+    plug_out = out_list.index(max(out_list))
+    multitap.pop(plug_out)
+
+    multitap.append(equip[i])
+    cnt += 1
+
+print(cnt)
