1a46a5dc5f4bd23ea2c07af0879e629a2c0d9631
kellyjung <jung55120@naver.com>
2022-03-18 11:39:32 +0900

    TIL 0318

---
+++ 04_nomadecoder/react-masterclass/src/App.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+function App() {
+  return (
+    <Father>
+      <Box bgColor="teal">
+        <Text>hello</Text>
+      </Box>
+      <Circle bgColor="tomato" />
+    </Father>
+  );
+}
+
+export default App;
