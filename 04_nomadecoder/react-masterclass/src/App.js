d00c5469add89f80cd015d6ed2928d2d430148a8
kellyjung <jung55120@naver.com>
2022-03-18 11:51:14 +0900

    TIL 0318

--- 04_nomadecoder/react-masterclass/src/App.js
+++ 04_nomadecoder/react-masterclass/src/App.js
@@ -18,13 +18,39 @@ const Circle = styled(Box)`
   border-radius: 50px;
 `;
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
+
 function App() {
   return (
     <Father>
+      {/*
+      // Box styled 하나로 여러가지 props 전달 받기
       <Box bgColor="teal">
         <Text>hello</Text>
       </Box>
-      <Circle bgColor="tomato" />
+      <Circle bgColor="tomato" /> */}
+
+      {/*
+      // button 태그 -> a 태그로 바꾸는 방법
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn> */}
+
+      {/* 
+      // styled로 attrs(속성) 적용하기
+      <Input />
+      <Input />
+      <Input /> */}
     </Father>
   );
 }
